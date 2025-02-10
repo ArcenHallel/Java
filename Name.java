91e4b1a0e1b4d658f19871166ac8a53eaeb0409e
Alfredo D <alfredo.a.diaz11@gmail.com>
2025-02-09 20:13:18 -0500

    progress

---
+++ Name.java
@@ -0,0 +1,7 @@
+public class Name {
+    public static void main(String[] args) {
+      String name = "Alfredo";
+      System.out.println(name);
+    }
+  }
+  
---
+++ myNum.java
@@ -0,0 +1,38 @@
+// public class myNum {
+//     public static void main(String[] args){
+//         int myNum = 15;
+//         System.out.println(myNum);
+//     }
+// }
+
+// // declare a variable without assigning the value, assign value later:
+
+// public class myNum {
+//     public static void main(String[] args) {
+//       int myNum;
+//       myNum = 15;
+//       System.out.println(myNum);
+//     }
+//   }
+  
+
+// //  Change the value of myNum from 15 to 20:
+
+// public class myNum {
+//     public static void main(String[] args) {
+//       int myNum = 15;
+//       myNum = 20;  // myNum is now 20
+//       System.out.println(myNum);
+//     }
+//   }
+  
+
+// constant (can not be changed and read-only)
+public class myNum {
+    public static void main(String[] args) {
+      final int myNum = 15;
+      myNum = 20; // will generate an error
+      System.out.println(myNum);
+    }
+  }
+  
